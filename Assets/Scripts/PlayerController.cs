a36e5fff4028a2d4c17502cd826aa840bc9db9d5
Ratta640 <s31043@pjwstk.edu.pl>
2025-06-10 18:44:17 +0200

    Game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,22 +6,54 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5;
     public Rigidbody2D rb;
-    public float jumpForce = 10;
+    public float jumpForce = 100;
     public float runSpeed = 10;
 
     public bool isGrounded;
     public GroundChecker groundChecker;
+    public Animator anim;
+    public SpriteRenderer spriteRenderer;
+    public PlayerHealth health;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        health = GetComponent<PlayerHealth>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
+
+
+
+
 
     // Update is called once per frame
     void Update()
     {
+        if (health.isDead) return;
+
         float moveInput = Input.GetAxis("Horizontal");
+
+        if (moveInput >= 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (moveInput < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        if (moveInput != 0)
+        {
+            anim.SetBool("IsRun", true);
+        }
+        else
+        {
+            anim.SetBool("IsRun", false);
+        }
+
         //Debug.Log($"Input pressed:{moveInput}");
+
         if (Input.GetKey(KeyCode.LeftShift))
         {
             rb.velocity = new Vector2(moveInput * runSpeed, rb.velocity.y);
